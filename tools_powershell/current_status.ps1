b315b95267764af6674c056596180068a8490877
Dolgormaa Sansarsaikhan <dolgormaadonna@gmail.com>
2026-06-27 20:50:25 -0600

    Writing status to file

--- tools_powershell/current_status.ps1
+++ tools_powershell/current_status.ps1
@@ -1,22 +1,37 @@
 # Run as Administrator
+$timestamp = Get-Date -Format "yyyyMMdd-HHmm"
+$out = "status-$timestamp.txt"
 
 Write-Host "[+] Checking for running processes..."
-Get-Process 
+"=== RUNNING PROCESSES ===" | Out-File $out
+Get-Process | Select-Object Name, Id, CPU, Path | Sort-Object CPU -Descending | Out-File $out -Append
 
 Write-Host "[+] Checking current services and states..."
-Get-Service 
+"=== CURRENT SERVICES ===" | Out-File $out -Append
+Get-Service | Where-Object Status -eq "Running"| Out-File $out -Append
 
 Write-Host "[+] Checking current active network connections..."
-Get-NetTCPConnection
+"=== ACTIVE NETWORK CONNECTIONS ===" | Out-File $out -Append
+Get-NetTCPConnection -State Established | Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort | Out-File $out -Append
 
 Write-Host "[+] Checking for local accounts..."
-Get-LocalUser
+"=== LOCAL ACCOUNTS ===" | Out-File $out -Append
+Get-LocalUser | Out-File $out -Append
 
 Write-Host "[+] Checking for current scheduled tasks..."
-Get-ScheduledTask
+"=== SCHEDULED TASKS ===" | Out-File $out -Append
+Get-ScheduledTask | Where-Object State -eq "Ready" | Select-Object TaskName, TaskPath | Out-File $out -Append
 
-#Write-Host "[+] Checking event logs..."
-#Get-WinEvent
+Write-Host "[+] Checking event logs..."
+"=== EVENT LOGS ===" | Out-File $out -Append
+#Log ins 
+Get-WinEvent -LogName Security | Where-Object Id -eq 4624 | Out-File $out -Append
+#System log offs
+Get-WinEvent -LogName Security | Where-Object Id -eq 4634 | Out-File $out -Append
+#Log offs
+Get-WinEvent -LogName Security | Where-Object Id -eq 4647 | Out-File $out -Append
+#Account creation
+Get-WinEvent -LogName Security | Where-Object Id -eq 4720 | Out-File $out -Append
 
 #Write-Host "[+] Checking system information and installed software..."
 #Get-WmiObject
